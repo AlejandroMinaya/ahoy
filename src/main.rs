bc99f7fb658a7bf3d203f2a818168fca13fb2d69
Juan A <j.alejandrominaya@gmail.com>
2025-09-13 19:54:16 +0200

    feat: Add command line support
    commit 7e56ea79a6d274b8ba836e92d6a686d02854f0a6
    Author: Juan A <j.alejandrominaya@gmail.com>
    Date:   Sat Sep 13 19:46:12 2025 +0200
    
        feat: Open file retrieve from prompt
    
    commit 3fce9af8ec89754cf840094512be1c54a83c9901
    Author: Juan A <j.alejandrominaya@gmail.com>
    Date:   Sat Sep 13 19:39:10 2025 +0200
    
        feat: Add clap for basic parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs::File, path::PathBuf};
+
+use clap::Parser;
+
+#[derive(Parser)]
+struct Args {
+    #[arg()]
+    program: PathBuf,
+}
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+    let program = File::open(&args.program)?;
+
+    Ok(())
 }
