f49ab50fae8694cdb9ba82e73a51c24af78b1064
Juan A. <j.alejandrominaya@gmail.com>
2025-09-16 23:19:16 +0200

    feat: Draw a rectangle in the middle of the screen

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
 use std::path::PathBuf;
 
 use crossterm::event::{self, Event};
-use ratatui::{self, Frame};
+use ratatui::{
+    self, Frame,
+    widgets::canvas::{Canvas, Map, Rectangle},
+};
 
 use clap::Parser;
 
@@ -11,7 +14,27 @@ struct Args {
     program: PathBuf,
 }
 fn render(frame: &mut Frame) {
-    frame.render_widget("hello world", frame.area());
+    let area = frame.area();
+    let area_width = f64::from(area.width);
+    let area_height = f64::from(area.height);
+    let rectangle_width = 10.0;
+    let rectangle_height = 5.0;
+    frame.render_widget(
+        Canvas::default()
+            .marker(ratatui::symbols::Marker::Block)
+            .paint(|ctx| {
+                ctx.draw(&Rectangle {
+                    x: (area_width - rectangle_width) / 2.0,
+                    y: (area_height - rectangle_height) / 2.0,
+                    width: rectangle_width,
+                    height: rectangle_height,
+                    color: ratatui::style::Color::Yellow,
+                });
+            })
+            .x_bounds([0.0, area_width])
+            .y_bounds([0.0, area_height]),
+        area,
+    );
 }
 fn main() -> anyhow::Result<()> {
     let mut terminal = ratatui::init();
