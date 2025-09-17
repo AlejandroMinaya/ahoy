883097b1c5f8780dcb810b70bad7bb4fad3471cb
Juan A. <j.alejandrominaya@gmail.com>
2025-09-18 00:21:35 +0200

    wip: Figure out that it cannot be a 1:1 mapping for aspect ratio

--- src/display.rs
+++ src/display.rs
@@ -32,15 +32,15 @@ impl AhoyDisplay for RatatuiAhoyDisplay {
             let area_height = f64::from(area.height);
             ratatui_frame.render_widget(
                 Canvas::default()
-                    .marker(ratatui::symbols::Marker::Block)
+                    .marker(ratatui::symbols::Marker::HalfBlock)
                     .paint(|ctx| {
                         for i in 0..32_u8 {
                             for j in 0..64_u8 {
                                 let row = 31_usize - usize::from(i);
                                 let pixel = frame[row] >> (63 - j);
                                 ctx.draw(&Rectangle {
-                                    y: f64::from(i) + (area_height - 32.0) / 2.0,
-                                    x: f64::from(j) + (area_width - 64.0) / 2.0,
+                                    y: f64::from(i),
+                                    x: f64::from(j),
                                     width: 1.0,
                                     height: 1.0,
                                     color: if pixel & 0b1 == 1 {
@@ -52,8 +52,8 @@ impl AhoyDisplay for RatatuiAhoyDisplay {
                             }
                         }
                     })
-                    .x_bounds([0.0, area_width])
-                    .y_bounds([0.0, area_height]),
+                    .x_bounds([0.0, 64.0])
+                    .y_bounds([0.0, 32.0]),
                 area,
             );
         })?;
