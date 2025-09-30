3911304ac9f2ff26d736a36e2643963cbd07e4f5
Juan A. <j.alejandrominaya@gmail.com>
2025-09-30 23:58:08 +0200

    feat: Decode set registers instructions

--- src/instructions.rs
+++ src/instructions.rs
@@ -3,6 +3,7 @@
 enum AhoyInstruction {
     ClearScreen = 0x00E0,
     Jump(u16),
+    SetRegister { register: u8, value: u8 },
     UnknownInstruction,
 }
 impl From<u16> for AhoyInstruction {
@@ -11,6 +12,14 @@ impl From<u16> for AhoyInstruction {
             0x00E0 => Self::ClearScreen,
             instruction => match instruction >> 0xC {
                 1 => Self::Jump(instruction & 0x0FFF),
+                6 => {
+                    let payload = instruction & 0x0FFF;
+                    Self::SetRegister {
+                        register: (payload >> 8) as u8,
+                        value: (payload & 0x0FF) as u8,
+                    }
+                }
+
                 _ => Self::UnknownInstruction,
             },
         }
@@ -31,7 +40,30 @@ mod tests {
         assert!(matches!(0x1FE0.into(), AhoyInstruction::Jump(0xFE0)));
         assert!(matches!(0x1ABC.into(), AhoyInstruction::Jump(0xABC)));
         assert!(matches!(0x1000.into(), AhoyInstruction::Jump(0x000)));
+    }
 
-        assert!(!matches!(0x2ABC.into(), AhoyInstruction::Jump(0xABC)));
+    #[test]
+    fn from_decode_set_register_instruction() {
+        assert!(matches!(
+            0x6FE0.into(),
+            AhoyInstruction::SetRegister {
+                register: 0xF,
+                value: 0xE0
+            }
+        ));
+        assert!(matches!(
+            0x6EEE.into(),
+            AhoyInstruction::SetRegister {
+                register: 0xE,
+                value: 0xEE
+            }
+        ));
+        assert!(matches!(
+            0x6A00.into(),
+            AhoyInstruction::SetRegister {
+                register: 0xA,
+                value: 0x00
+            }
+        ));
     }
 }
