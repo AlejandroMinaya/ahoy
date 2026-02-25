0c879162eadeeae2924ea735dd276c408b938f33
Juan Alcantara <jalcanmi@mercadona.es>
2026-02-25 23:43:41 +0100

    tutorial(challenge): Use triangle position for color info

--- src/display/native_display/mod.rs
+++ src/display/native_display/mod.rs
@@ -157,6 +157,7 @@ impl State {
         let view = output
             .texture
             .create_view(&wgpu::TextureViewDescriptor::default());
+
         let mut encoder = self
             .device
             .create_command_encoder(&wgpu::CommandEncoderDescriptor {
@@ -169,12 +170,7 @@ impl State {
                     view: &view,
                     resolve_target: None,
                     ops: wgpu::Operations {
-                        load: wgpu::LoadOp::Clear(wgpu::Color {
-                            r: 0.1,
-                            g: 0.2,
-                            b: 0.3,
-                            a: 1.0,
-                        }),
+                        load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
                         store: wgpu::StoreOp::Store,
                     },
                     depth_slice: None,
